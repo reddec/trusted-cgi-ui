7e9a9bffd92795e19c2a05e61e50f7ea92b83ed8
Alexander Baryshnikov <dev@baryshnikov.net>
2020-05-26 20:20:12 +0800

    add actions support

--- src/components/widgets/Actions.vue
+++ src/components/widgets/Actions.vue
@@ -2,17 +2,25 @@
   <transition>
     <q-linear-progress indeterminate v-if="loading"/>
     <h5 v-else-if="!actions || actions.length === 0">actions not found</h5>
-    <q-list bordered v-else>
-      <q-item-label header>Detected actions</q-item-label>
-      <q-item v-for="name in actions" :key="name">
-        <q-item-section>
-          <q-item-label caption>{{name}}</q-item-label>
-        </q-item-section>
-        <q-item-section side>
-          <q-btn icon="play_arrow" :loading="invoking" @click="invoke(name)" flat dense round/>
-        </q-item-section>
-      </q-item>
-    </q-list>
+    <div v-else>
+      <q-list bordered>
+        <q-item-label header>Detected actions</q-item-label>
+        <q-item v-for="name in actions" :key="name">
+          <q-item-section>
+            <q-item-label caption>{{name}}</q-item-label>
+          </q-item-section>
+          <q-item-section side>
+            <q-btn icon="play_arrow" :loading="invoking" @click="invoke(name)" flat dense round/>
+          </q-item-section>
+        </q-item>
+      </q-list>
+      <br/>
+      <q-card bordered square flat v-if="result">
+        <q-card-section>
+          <pre style="white-space: pre-line; word-wrap: break-word; overflow-x: auto">{{result}}</pre>
+        </q-card-section>
+      </q-card>
+    </div>
   </transition>
 </template>
 
@@ -28,6 +36,7 @@
       return {
         loading: false,
         invoking: false,
+        result: '',
         actions: []
       }
     },
@@ -37,8 +46,9 @@
     methods: {
       async invoke(name) {
         this.invoking = true;
+        this.result = '';
         try {
-          await client.invoke(this.token, this.selectedApp?.uid, name)
+          this.result = await client.invoke(this.token, this.selectedApp?.uid, name)
         } catch (e) {
           console.error(e)
         } finally {
