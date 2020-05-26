3051db383f7d99fb322e443c69443879087b54ec
Alexander Baryshnikov <dev@baryshnikov.net>
2020-05-26 12:56:09 +0800

    mobile friendly app page

--- src/pages/App.vue
+++ src/pages/App.vue
@@ -1,63 +1,59 @@
 <template>
   <q-page class="q-pa-md content-stretch">
     <q-linear-progress indeterminate v-if="loadingApp"/>
-    <div class="column content-stretch" v-else-if="selectedApp">
-      <div>
-        <q-chip :icon="selectedApp.manifest.public ? 'public' : 'security'">{{ api }}</q-chip>
-      </div>
-      <div class="row content-stretch">
-        <q-card class="col-grow " flat bordered>
-          <q-tabs
-            dense
-            v-model="tab"
-            class="text-grey"
-            active-color="primary"
-            indicator-color="primary"
-          >
-            <q-tab name="overview" label="Overview" icon="web_asset"/>
-            <q-tab name="files" label="Files" icon="folder"/>
-            <q-tab name="stats" label="Stats" icon="bar_chart"/>
-            <q-tab name="launcher" label="Launcher" icon="launch"/>
-            <q-tab name="playground" label="Playground" icon="play_arrow"/>
-            <q-tab name="limits" label="Limits" icon="speed"/>
-            <q-tab name="mapping" label="Mapping" icon="shuffle"/>
-            <q-tab name="security" label="Security" icon="security"/>
-          </q-tabs>
+    <div v-else-if="selectedApp">
+      <q-tabs
+        dense
+        v-model="tab"
+        class="text-grey"
+        active-color="primary"
+        indicator-color="primary"
+      >
+        <q-tab name="overview" label="Overview" icon="web_asset"/>
+        <q-tab name="files" label="Files" icon="folder"/>
+        <q-tab name="stats" label="Stats" icon="bar_chart"/>
+        <q-tab name="launcher" label="Launcher" icon="launch"/>
+        <q-tab name="playground" label="Playground" icon="play_arrow"/>
+        <q-tab name="limits" label="Limits" icon="speed"/>
+        <q-tab name="mapping" label="Mapping" icon="shuffle"/>
+        <q-tab name="security" label="Security" icon="security"/>
+      </q-tabs>
 
-          <q-separator/>
+      <q-separator/>
 
-          <q-tab-panels v-model="tab" animated>
-            <q-tab-panel name="overview">
-              <Overview/>
-            </q-tab-panel>
-            <q-tab-panel name="stats">
-              <AppStats/>
-            </q-tab-panel>
-            <q-tab-panel name="launcher">
-              <Launcher/>
-            </q-tab-panel>
-            <q-tab-panel name="playground">
-              <Playground/>
-            </q-tab-panel>
-            <q-tab-panel name="files">
-              <div class="row">
-                <FileBrowser :name="name" @openFile="(info)=> selectedItem = info"/>
-                <FileEditor :name="name" :file="filepath"/>
-              </div>
-            </q-tab-panel>
-            <q-tab-panel name="limits">
-              <div class="text-h6">Limits</div>
-              <Limits/>
-            </q-tab-panel>
-            <q-tab-panel name="mapping">
-              <Mapping/>
-            </q-tab-panel>
-            <q-tab-panel name="security">
-              <Security/>
-            </q-tab-panel>
-          </q-tab-panels>
-        </q-card>
-      </div>
+      <q-tab-panels v-model="tab" animated>
+        <q-tab-panel name="overview">
+          <div>
+            <q-chip :icon="selectedApp.manifest.public ? 'public' : 'security'">{{ api }}</q-chip>
+          </div>
+          <Overview/>
+        </q-tab-panel>
+        <q-tab-panel name="stats">
+          <AppStats/>
+        </q-tab-panel>
+        <q-tab-panel name="launcher">
+          <Launcher/>
+        </q-tab-panel>
+        <q-tab-panel name="playground">
+          <Playground/>
+        </q-tab-panel>
+        <q-tab-panel name="files">
+          <div class="row">
+            <FileBrowser :name="name" @openFile="(info)=> selectedItem = info"/>
+            <FileEditor :name="name" :file="filepath"/>
+          </div>
+        </q-tab-panel>
+        <q-tab-panel name="limits">
+          <div class="text-h6">Limits</div>
+          <Limits/>
+        </q-tab-panel>
+        <q-tab-panel name="mapping">
+          <Mapping/>
+        </q-tab-panel>
+        <q-tab-panel name="security">
+          <Security/>
+        </q-tab-panel>
+      </q-tab-panels>
     </div>
   </q-page>
 </template>
